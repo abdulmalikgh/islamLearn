3cb90e76603a42194a8f9bf2d516dc84d6bc5451
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-19 10:24:10 +0800

    Delete components directory

--- components/helperviews/AppText.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Text } from "react-native";
-
-function AppText({ children, textStyle = {} }) {
-  return <Text style={[{ textAlign: "center" }, textStyle]}>{children}</Text>;
-}
-
-export default AppText;
--- components/helperviews/Loading.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { ActivityIndicator } from "react-native";
-
-export default function Loading({ loading }) {
-  return <ActivityIndicator size="large" color="black" animating={loading} />;
-}
